358ca066c85cc9a29eee7fa14d8dc1a2ff8dd67e
erivelon madureira <erivelton@eriveltonmadureira.com>
2025-08-22 14:59:06 -0300

    adicionado logica boolean

---
+++ Fundamentos/templateString.js
@@ -0,0 +1,12 @@
+const nome = "rebeca";
+const concatenacao = "Olá " + nome + "!";
+const template = `
+    Olá
+    ${nome}!
+    `;
+console.log(concatenacao, template);
+
+// expression ...
+console.log(`1 + 1 = ${1 + 1} `);
+
+
